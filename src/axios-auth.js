04ab59e82d0718046469a874050c3518b4229a90
TTeRzY <marin.terziyski@gmail.com>
2018-07-23 17:38:30 +0300

    Adding files into the repo

---
+++ src/axios-auth.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
+})
+
+export default instance
---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+    <p>Your email address: {{ email }}</p>
+  </div>
+</template>
+
+<script>
+
+  export default {
+   computed: {
+     email () {
+       return !this.$store.getters.user ? false : this.$store.getters.user.email
+     }
+   },
+    created () {
+      this.$store.dispatch('fetchUser')
+    }
+  }
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
